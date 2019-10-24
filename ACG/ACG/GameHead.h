3a339407136dc3769df0756358d3347583e73ffb
xxxikuraxxx@gmail.com <xxxikuraxxx@gmail.com>
2019-10-24 14:37:29 +0900

    ゲームスタートから再生に変更

--- ACG/ACG/GameHead.h
+++ ACG/ACG/GameHead.h
@@ -72,5 +72,5 @@ struct UserData
 
 //シーンスタートクラス---------------------------
 //ゲーム開始時のシーンクラス登録
-#define SET_GAME_START CSceneGameOver  
+#define SET_GAME_START CSceneGameStart
 //-----------------------------------------------
