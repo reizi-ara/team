3f47590561865a49c3dbcc7087e418cd425509a2
大暉 永原 <h1201919007@hiratagakuen.onmicrosoft.com>
2019-10-24 11:06:22 +0900

    スタート背景描画

--- ACG/ACG/ObjGameStart.cpp
+++ ACG/ACG/ObjGameStart.cpp
@@ -1,5 +1,7 @@
 //使用するヘッダーファイル
+
 #include"GameL\DrawFont.h"
+#include "GameL/DrawTexture.h"
 #include"GameL\WinInputs.h"
 #include"GameL\SceneManager.h"
 
@@ -36,9 +38,27 @@ void CObjGameStart::Action()
 void CObjGameStart::Draw()
 {
 	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
+	
+
+
+	RECT_F src;//描画元切り取り位置
+	RECT_F dst;//描画先表示位置
+
+	//切り取り位置の設定
+	src.m_top = 0.0f;
+	src.m_left = 0.0f;
+	src.m_right = 960.0f;
+	src.m_bottom = 450.0f;
+
+	//背景1の位置を設定し描画
+	dst.m_top = 0.0f;
+	dst.m_left = 0.0f;
+	dst.m_right = 800.0f;
+	dst.m_bottom = 600.0f;
+	Draw::Draw(2, &src, &dst, c, 0.0f);
+
 	Font::StrDraw(L"かぼちゃの館", 240, 220, 32, c);
 
 	Font::StrDraw(L"GameStart : PushEnterKey", 210, 380, 32, c);
-
 }
 
