cedc924d4af6989f4f0c18c20ac4cd625447bcad
GA1B <GA1B@DEV2-11>
2019-10-25 10:07:18 +0900

    ゲームヘッドに追加したよ

---
+++ ACG/ACG/ObjMenu.h
@@ -0,0 +1,24 @@
+#pragma once
+//使用するヘッダーファイル
+#include "GameL\SceneObjManager.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//シーン：ゲームタイトル
+class CObjGameMenu : public CObj
+{
+public:
+	CObjGameMenu() {};
+	~CObjGameMenu() {};
+	void Menu();
+	void Init();		//イニシャライズ
+	void Action();		//アクション
+	void Draw();		//ドロー
+private:
+	bool m_key_flag;//キーフラグ
+	int lavel_select;//選択時協調バー用
+	bool lavel_button;//一回発動用
+	bool lavel_button2;//一回発動用
+
+};
