214d28f08912e3faafe7026c926c10c8e147afaf
xxxikuraxxx@gmail.com <xxxikuraxxx@gmail.com>
2019-12-19 12:01:44 +0900

    暗くしてみたけど不評なら戻す

--- ACG/ACG/ObjBlock.cpp
+++ ACG/ACG/ObjBlock.cpp
@@ -348,8 +348,8 @@ void CObjBlock::BlockDraw(float x, float y, RECT_F* dst, float c[],int s)
 void CObjBlock::Draw()
 {
 	//描画カラー情報
-	float c[4]  = { 1.0f,1.0f,1.0f,1.0f };
-	float cr[4] = { 0.7f,0.7f,0.7f,1.0f };
+	float c[4]  = { 0.5f,0.5f,0.5f,1.0f };
+	float cr[4] = { 0.3f,0.3f,0.3f,1.0f };
 
 	RECT_F src;//描画元切り取り位置
 	RECT_F dst;//描画先表示位置
@@ -365,9 +365,6 @@ void CObjBlock::Draw()
 	dst.m_right = WINDOW_SIZE_W;
 	dst.m_bottom = WINDOW_SIZE_H;
 	Draw::Draw(5, &src, &dst, c, 0.0f);
-
-
-
 
 	for (int i = 0; i < 10; i++)
 	{
--- ACG/ACG/Objhand.cpp
+++ ACG/ACG/Objhand.cpp
@@ -146,7 +146,7 @@ void CObjhand::Draw()
 	};
 
 	//描画カラー情報
-	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
+	float c[4] = { 0.7f,0.7f,0.7f,1.0f };
 
 	RECT_F src;//描画元切り取り位置
 	RECT_F dst;//描画先表示位置
